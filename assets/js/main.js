d95885a1e776b9f562b1fd319b2b003d2ebb34f7
mahbubur1 <mdmahbuborr462@gail.com>
2022-06-10 18:55:08 +0600

    dark and light theme done

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,7 +178,7 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
